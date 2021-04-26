810789f6a4c1c3f903f98eebb7bb9b4cc4cc201c
Neil Justice <njjustice@gmail.com>
2021-04-26 13:19:06 -0500

    Add '# pylint: disable=consider-using-with' to WhittlerTests/ParallelProcess.py

--- WhittlerTests/ParallelProcess.py
+++ WhittlerTests/ParallelProcess.py
@@ -6,7 +6,7 @@ from WhittlerTests import ProcessThread
 
 def run_parallel_processpoolexecutor(func: Any, iterable: Any) -> bool:
    cpuCount = multiprocessing.cpu_count()
-   processPoolExecutor = concurrent.futures.ProcessPoolExecutor(cpuCount)
+   processPoolExecutor = concurrent.futures.ProcessPoolExecutor(cpuCount) # pylint: disable=consider-using-with
    exitCodes = processPoolExecutor.map(func, iterable)
    processPoolExecutor.shutdown(wait=True)
    allCommandsSucceeded = not any(exitCodes)
