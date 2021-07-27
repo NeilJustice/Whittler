4cd38a6195f0fecd631180e50871282db25b23d4
Neil Justice <njjustice@gmail.com>
2021-07-27 15:17:03 -0500

    Update Windows Jenkins scripts to temporarily set env:PYTHONPATH to . and then restore env:PYTHONPATH to its original value

--- JenkinsJobs/Windows/Whittler.ps1
+++ JenkinsJobs/Windows/Whittler.ps1
@@ -1,3 +1,5 @@
+$originalPythonPath = $env:PYTHONPATH
 $env:PYTHONPATH="."
 python.exe WhittlerTests\MypyFlake8PylintRunTestsWithCoverage.py --run-tests-with-coverage-python-file=WhittlerTests/RunAllWithCoverage.py
+$env:PYTHONPATH=$originalPythonPath
 exit $LastExitCode
